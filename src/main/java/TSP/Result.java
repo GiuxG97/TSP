22c9fc3d1acf689574c5702460f8943dda76baad
Luca Giussani <luca.giussani@student.supsi.ch>
2019-03-30 19:28:44 +0100

    preparazione salvataggio e confronto risultati

---
+++ src/main/java/TSP/Result.java
@@ -0,0 +1,31 @@
+package TSP;
+
+public class Result {
+    private long time;
+    private double error;
+    private long seed;
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public double getError() {
+        return error;
+    }
+
+    public void setError(double error) {
+        this.error = error;
+    }
+
+    public long getSeed() {
+        return seed;
+    }
+
+    public void setSeed(long seed) {
+        this.seed = seed;
+    }
+}
--- src/main/java/application/MainNuovaGui.java
+++ src/main/java/application/MainNuovaGui.java
@@ -1,14 +1,9 @@
 package application;
 
-import TSP.City;
-import TSP.Timer;
-import TSP.Tour;
-import TSP.TourManager;
+import TSP.*;
 import TSP.simulatedAnnealing.NearestNeighbour;
 import TSP.simulatedAnnealing.SimulatedAnnealing;
 import TSP.simulatedAnnealing.TwoOpt;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 import parser.FileReader;
 import parser.Parser;
 
@@ -21,7 +16,7 @@ public class MainNuovaGui {
     private static Tour tour;
 
 
-    public static String getPlotCommand(String path){
+    public static String getPlotCommand(String path) {
         return String.format("plot \"%1$s\" using 1:2 " +
                 "with points pointtype 5, " +
                 "\"%1$s\" using 1:2 with l t \"S1\"", path);
@@ -39,37 +34,49 @@ public class MainNuovaGui {
     }
 
     public static void main(String[] args) {
-        double error;
-        double minError;
-        int minSeed;
-        List<Double> minErrors = new ArrayList<>();
-        List<Integer> minSeeds = new ArrayList<>();
         InputStream inputFile;
         String[] filesName = {"eil76.tsp", "kroA100.tsp", "ch130.tsp", "d198.tsp", "lin318.tsp", "pr439.tsp", "pcb442.tsp", "rat783.tsp", "u1060.tsp", "fl1577.tsp"};
-        for (String name : filesName) {
-            minError = Double.MAX_VALUE;
-            minSeed = 0;
-//            System.err.println(f.getName());
-            for (int i = 0; i < 1; i++) {
-                inputFile = chooseResource(name);
-                error = computeAlgorithms(System.currentTimeMillis(), inputFile);
-                if (error < minError) {
-                    minError = error;
-                    minSeed = i;
+        long seed;
+        Result result;
+        while (true) {
+            for (int i = 0; i < filesName.length; i++) {
+                inputFile = chooseResource(filesName[i]);
+                seed = System.currentTimeMillis();
+                result = computeAlgorithms(seed, inputFile);
+                try {
+                    if (checkResults(filesName[i], result)) {
+                        printFile(result, filesName[i]);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+
             }
-            minErrors.add(minError);
-            minSeeds.add(minSeed);
         }
-
-        print();
-
 
     }
 
+    private static void printFile(Result result, String fileName) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter(new FileOutputStream(fileName + ".txt", false));
+        writer.println(result.getError());
+        writer.println(result.getSeed());
+        writer.println(result.getTime());
+        writer.close();
+    }
 
-    private static double computeAlgorithms(long seed, InputStream inputFile){
+    private static boolean checkResults(String fileName, Result result) throws FileNotFoundException {
+        FileReader fileReader = new FileReader(fileName);
+        Result oldResult = fileReader.readFileResult();
+        if (result.getError() < oldResult.getError())
+            return true;
+
+        return false;
+
+    }
+
+    private static Result computeAlgorithms(long seed, InputStream inputFile) {
         Timer timer = new Timer();
+        Result result = new Result();
         timer.startTimer();
         FileReader fileReader = new FileReader();
         fileReader.setInputFile(inputFile);
@@ -95,18 +102,22 @@ public class MainNuovaGui {
         tour = tourSimulatedAnnealing;
 
         timer.stopTimer();
-        System.out.println("Nearest neighbour");
-        tourNearest.print();
-        System.out.println("Two-Opt after nearest neighbour");
-        tourTwoOpt.print();
-        System.out.println("Simulated annealing:");
-        tourSimulatedAnnealing.print();
-        System.out.println("Best distance: " + parser.getBestKnown());
+//        System.out.println("Nearest neighbour");
+//        tourNearest.print();
+//        System.out.println("Two-Opt after nearest neighbour");
+//        tourTwoOpt.print();
+//        System.out.println("Simulated annealing:");
+//        tourSimulatedAnnealing.print();
+//        System.out.println("Best distance: " + parser.getBestKnown());
 
-        double error = (double) (tour.getTotalDistance() - parser.getBestKnown()) / (double)parser.getBestKnown();
-        System.out.println("Error: " + error*100 + "%");
+        double error = (double) (tour.getTotalDistance() - parser.getBestKnown()) / (double) parser.getBestKnown();
+//        System.out.println("Error: " + error * 100 + "%");
+        result.setError(error);
+        result.setSeed(seed);
+        result.setTime(timer.getElapsedTime());
 
-        return error;
+
+        return result;
 
     }
 
@@ -119,7 +130,7 @@ public class MainNuovaGui {
             BufferedWriter b = new BufferedWriter(w);
 
             List<City> tourCities = new ArrayList<>();
-            for (int i=0; i<tour.size(); i++)
+            for (int i = 0; i < tour.size(); i++)
                 tourCities.add(cities.get(tour.get(i)));
 
             for (City city : tourCities)
